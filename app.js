b42dc9392742c747b1f860d3243d2f9bcd3a01ce
praveen mishra <praveen2616@gmail.com>
2021-06-01 16:41:11 +0530

    committed

--- app.js
+++ app.js
@@ -84,6 +84,6 @@ app.post("/failure" , function(req,res) {
 
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000 , function(){
     console.log("Server started!");
 });
